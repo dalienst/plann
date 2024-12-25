412819c2fba4faf7005b56732bc905763ff82e69
dalienst <oduordalienst@gmail.com>
2024-12-25 14:34:29 +0300

    style registration page

--- app/auth/signup/page.jsx
+++ app/auth/signup/page.jsx
@@ -10,119 +10,118 @@ import toast from "react-hot-toast";
 function SignUp() {
   const [loading, setIsLoading] = useState(false);
   const router = useRouter();
+
   return (
-    <Formik
-      initialValues={{
-        email: "",
-        password: "",
-        confirmPassword: "",
-      }}
-      validationSchema={RegistrationSchema}
-      onSubmit={async (values) => {
-        setIsLoading(true);
-        try {
-          await signUp(values);
-          toast.success("Account created successfully");
-          router.push("/auth/verify");
-        } catch (error) {
-          if (
-            error?.response?.data?.email[0] ||
-            error?.response?.data?.username[0]
-          ) {
-            toast.error("User already exists");
-          } else {
-            toast.error("Registration Failed");
+    <div className="auth-screen">
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+          confirmPassword: "",
+        }}
+        validationSchema={RegistrationSchema}
+        onSubmit={async (values) => {
+          setIsLoading(true);
+          try {
+            await signUp(values);
+            toast.success("Account created successfully");
+            router.push("/auth/verify");
+          } catch (error) {
+            if (
+              error?.response?.data?.email[0] ||
+              error?.response?.data?.username[0]
+            ) {
+              toast.error("User already exists");
+            } else {
+              toast.error("Registration Failed");
+            }
+          } finally {
+            setIsLoading(false);
           }
-        } finally {
-          setIsLoading(false);
-        }
-      }}
-    >
-      {({ touched }) => (
-        <div className="container-fluid">
-          <div className="auth-screen">
-            <Form className="mt-5 shadow-lg p-4 rounded bg-white">
-              <h2 className="text-center mb-4">Sign Up</h2>
-              <div className="mb-3">
-                <label htmlFor="email" className="form-label">
-                  Enter Email
-                </label>
-                <Field
-                  type="email"
-                  className="form-control"
-                  id="email"
+        }}
+      >
+        {({ touched }) => (
+          <Form className="shadow p-4 bg-white">
+            <h2 className="text-center mb-4">Sign Up</h2>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Enter Email
+              </label>
+              <Field
+                type="email"
+                className="form-control rounded-0"
+                id="email"
+                name="email"
+              />
+              {touched.email && (
+                <ErrorMessage
                   name="email"
+                  component="p"
+                  className="text-danger mt-2"
                 />
-                {touched.email && (
-                  <ErrorMessage
-                    name="email"
-                    component="p"
-                    className="text-danger mt-2"
-                  />
-                )}
-              </div>
+              )}
+            </div>
 
-              <div className="mb-3">
-                <label htmlFor="password" className="form-label">
-                  Enter Password
-                </label>
-                <Field
-                  type="password"
-                  className="form-control"
-                  id="password"
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Enter Password
+              </label>
+              <Field
+                type="password"
+                className="form-control rounded-0"
+                id="password"
+                name="password"
+              />
+              {touched.password && (
+                <ErrorMessage
                   name="password"
+                  component="p"
+                  className="text-danger mt-2"
                 />
-                {touched.password && (
-                  <ErrorMessage
-                    name="password"
-                    component="p"
-                    className="text-danger mt-2"
-                  />
-                )}
-              </div>
+              )}
+            </div>
 
-              <div className="mb-3">
-                <label htmlFor="confirmPassword" className="form-label">
-                  Confirm Password
-                </label>
-                <Field
-                  type="password"
-                  className="form-control"
-                  id="confirmPassword"
+            <div className="mb-3">
+              <label htmlFor="confirmPassword" className="form-label">
+                Confirm Password
+              </label>
+              <Field
+                type="password"
+                className="form-control rounded-0"
+                id="confirmPassword"
+                name="confirmPassword"
+              />
+              {touched.confirmPassword && (
+                <ErrorMessage
                   name="confirmPassword"
+                  component="p"
+                  className="text-danger mt-2"
                 />
-                {touched.confirmPassword && (
-                  <ErrorMessage
-                    name="confirmPassword"
-                    component="p"
-                    className="text-danger mt-2"
-                  />
-                )}
-              </div>
+              )}
+            </div>
 
-              <div className="d-flex justify-content-center">
-                <button
-                  type="submit"
-                  className="btn btn-dark btn-lg w-100"
-                  disabled={loading}
-                >
-                  {loading ? "Loading..." : "Sign Up"}
-                </button>
-              </div>
+            <div className="d-flex justify-content-center">
+              <button
+                type="submit"
+                className="btn btn-dark btn-sm rounded-0 w-100"
+                disabled={loading}
+              >
+                {loading ? "Loading..." : "Sign Up"}
+              </button>
+            </div>
 
-              <div className="text-center mt-3">
-                <p>
-                  Already have an account?{" "}
-                  <a href="/auth/login" className="text-primary">
-                    Login
-                  </a>
-                </p>
-              </div>
-            </Form>
-          </div>
-        </div>
-      )}
-    </Formik>
+            <div className="text-center mt-3">
+              <p>
+                Already have an account?{" "}
+                <a href="/auth/login" className="text-primary">
+                  Login
+                </a>
+              </p>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 }
 
